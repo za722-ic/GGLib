9a5a4f05a876abe24fe39fc5e9f43cae79b659b3
za722-ic <136204809+za722-ic@users.noreply.github.com>
2026-05-25 17:08:51 +0100

    Update Demo_Sandbox to be a simple application

--- Demo_Sandbox/Main.cpp
+++ Demo_Sandbox/Main.cpp
@@ -1,9 +1,37 @@
-#include "Demo.h"
+#include "GG/Core/Application.h"
+
+class MyApp : public GG::Application
+{
+	bool onInit() override
+	{
+		// set up window
+		window.setTitle("MyApp");
+		window.setResizable(false);
+		window.setSize(300, 300);
+
+		// tell library set up was successful
+		return true;
+	}
+
+	void onLoop() override
+	{
+		// clear screen
+		canvas.setColor(0x58, 0x5B, 0x65);
+		canvas.clear();
+
+		// draw UI
+		canvas.setColor(255, 0, 0);
+		canvas.fillRect(100, 100, 100, 100);
+
+		// update screen
+		canvas.present();
+	}
+};
 
 int main(int argc, char* args[])
 {
-    Demo demo;
-    demo.begin();
+	MyApp app;
+	app.begin();
 
-    return 0;
+	return 0;
 }
